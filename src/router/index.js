017e3b8e4d0c15dc5dff2780df2c7df3cde3e6d8
Chartchai Doungsa-ard <chartchai.d@cmu.ac.th>
2022-09-15 18:33:27 +0700

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,104 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import EventListView from '@/views/EventListView.vue'
+import EventEditView from '@/views/event/EventEditView.vue'
+import EventRegisterView from '@/views/event/EventRegisterView.vue'
+import AboutView from '../views/AboutView.vue'
+import EventLayoutView from '@/views/event/EventLayoutView.vue'
+import EventDetailView from '@/views/event/EventDetailView.vue'
+import NotFoundView from '@/views/NotFoundView.vue'
+import NetWorkErrorView from '@/views/NetworkErrorView.vue'
+import NProgress from 'nprogress'
+import GStore from '@/store'
+import EventService from '@/services/EventService'
+const routes = [
+  {
+    path: '/',
+    name: 'EventList',
+    component: EventListView,
+    props: (route) => ({ page: parseInt(route.query.page) || 1 })
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: AboutView
+  },
+  {
+    path: '/event/:id',
+    name: 'EventLayoutView',
+    component: EventLayoutView,
+    beforeEnter: (to) => {
+      return EventService.getEvent(to.params.id)
+        .then((response) => {
+          GStore.event = response.data
+        })
+        .catch((error) => {
+          if (error.response && error.response.start == 404) {
+            return {
+              name: '404Resource',
+              parames: { resource: 'event' }
+            }
+          } else {
+            return { name: 'NetworkError' }
+          }
+        })
+    },
+    props: true,
+    children: [
+      {
+        path: '',
+        name: 'EventDetails',
+        component: EventDetailView,
+        props: true
+      },
+      {
+        path: 'register',
+        name: 'EventRegister',
+        props: true,
+        component: EventRegisterView
+      },
+      {
+        path: 'edit',
+        name: 'EventEdit',
+        props: true,
+        component: EventEditView
+      }
+    ]
+  },
+  {
+    path: '/404/:resource',
+    name: '404Resource',
+    component: NotFoundView,
+    props: true
+  },
+  {
+    path: '/:catchAll(.*)',
+    name: 'NotFound',
+    component: NotFoundView
+  },
+  {
+    path: '/network-error',
+    name: 'NetworkError',
+    component: NetWorkErrorView
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0 }
+    }
+  }
+})
+router.beforeEach(() => {
+  NProgress.start()
+})
+
+router.afterEach(() => {
+  NProgress.done()
+})
+
+export default router
